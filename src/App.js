8a7364fb50326f75ffad86cea39115327bd78f10
1000peach <_970813@daum.net>
2021-06-13 15:16:53 +0900

    [feat] 공통 Header 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import styled from "styled-components";
+
+import Header from "./components/Header";
 import Home from "./pages/Home";
 
 import "./reset.css";
@@ -7,6 +9,7 @@ import "./reset.css";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Main>
         <Switch>
           <Route exact path="/" component={Home} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return <Wrapper>Angular / Angular-cli</Wrapper>;
+}
+
+const Wrapper = styled.header`
+  margin: 20px 0;
+  text-align: center;
+`;
+
+export default Header;
