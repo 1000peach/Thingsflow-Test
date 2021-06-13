1d615a9e63d2af6f05cbfecd63dc6335e98d1cb0
1000peach <_970813@daum.net>
2021-06-13 15:43:35 +0900

    [feat] getIssueList get api 테스트

--- src/api/issueAPI.js
+++ src/api/issueAPI.js
@@ -3,6 +3,6 @@ import { get } from "./instance";
 export default class IssueAPI {
   static getIssueList(params) {
     const { owner, repo } = params;
-    return get(`/repos/${owner}/${repo}/issues`, params);
+    return get(`/repos/${owner}/${repo}/issues?sort=comments`, params);
   }
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+
+import IssueAPI from "../api/issueAPI";
 
 import List from "../components/List";
 
 function Home() {
+  const getIssueList = async () => {
+    const data = await IssueAPI.getIssueList({
+      owner: "angular",
+      repo: "angular-cli"
+    });
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getIssueList();
+  }, []);
+
   return (
     <Container>
       <List />
