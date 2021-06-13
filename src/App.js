90be9aefccb5cc1ac0ca30fca23d9c3d43ad645e
1000peach <_970813@daum.net>
2021-06-13 15:08:14 +0900

    [feat] Home route 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>이슈 목록</div>;
+}
+
+export default Home;
