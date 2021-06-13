df22523a398f1337a4703784b715aed9824608b3
1000peach <_970813@daum.net>
2021-06-13 15:13:46 +0900

    [feat] reset.css 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import styled from "styled-components";
 import Home from "./pages/Home";
+
+import "./reset.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
+      <Main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Main>
     </BrowserRouter>
   );
 }
+
+const Main = styled.main`
+  padding: 0 20px;
+`;
 
 export default App;
